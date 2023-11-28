f92ff6b706640a2220afb24a4f61cbe63cee095e
Marcus Matthews <mlmatthews75@gmail.com>
2023-11-27 19:28:24 -0500

    added connection code

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// Import Dependencies
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// configure PORT
 const PORT = process.env.PORT || 3001;
+// create express app
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+//database connection
 db.once('open', () => {
+    // start the server
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
     });
