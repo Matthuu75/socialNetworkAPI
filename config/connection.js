43d2ece8db88588decd18e3b99af71afa24a1c84
Marcus Matthews <mlmatthews75@gmail.com>
2023-11-27 19:32:49 -0500

    added code to models files

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/socialNetworkAPI", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/socialNetworkAPI"), 
 module.exports = mongoose.connection;
